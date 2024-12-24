7a6cfd1ac94905cb852a17eee8f950a0ea3df8fc
Awqa99 <tosaco7317@cctoolz.com>
2024-12-24 14:18:46 +0530

    Delete home directory

--- home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def login_page(request):
-    return render(request, 'login.html')
-
-def signup_page(request):
-    return render(request, 'signup.html')
